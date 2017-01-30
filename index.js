6de5353d0b92a21e997b4ea59e019499b270c83f
Tom Lee <tlee418@g.ucla.edu>
2017-01-29 16:37:04 -0800

    Get user full name and location
    * username is hardcoded
    * need to get challenges and profile picture link

--- index.js
+++ index.js
@@ -12,9 +12,28 @@ app.get('/users/:name', function(req, res){
   request(url, function(error, response, body) {
     if (!error && response.statusCode == 200) {
       var $ = cheerio.load(body);
+      var name, location, longestStreak, currentStreak, challenges;
+
+      var json = { name : "", profileImage: "", location : "", longestStreak : "", currentStreak : "",
+                   challenges : "" };
+
+      $('.public-profile-img').filter(function(){
+        var data = $(this);
+
+        profileImage = data[0].src
+        name = data.next().next().text();
+        location = data.parent().find('.flat-top.wrappable').slice(1).text();
+
+        json.name = name;
+        json.profileImage = profileImage;
+        json.location = location;
+      })
+
+      res.setHeader('content-type', 'application/json');
+      res.send(JSON.stringify(json, null, 3));
     }
-  }
-})
+  });
+});
 
 app.listen(app.get('port'), function(){
   console.log('Express start press Ctrl-C to terminate');
